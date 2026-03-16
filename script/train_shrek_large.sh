a058a6622282672a3120453ab1d387d5ac5633a4
Thomas Heim <wearedreamin@gmail.com>
2026-03-16 18:21:22 +0100

    feat: Add early stopping parameter to training scripts for shrek_large and shrek_tiny models

--- script/train_shrek_large.sh
+++ script/train_shrek_large.sh
@@ -22,4 +22,5 @@ python3 pretrain.py \
     puzzle_emb_lr=1e-4 \
     weight_decay=1.0 \
     puzzle_emb_weight_decay=1.0 \
-    run_name=shrek-large-sudoku-extreme
+    run_name=shrek-large-sudoku-extreme \
+    early_stopping_patience=15
