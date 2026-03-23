de2e8fc44bf766ce65c529bb9725cc5d4db16246
Thomas Heim <wearedreamin@gmail.com>
2026-03-23 18:07:05 +0100

    feat: Update training parameters in HRM and augmented HRM scripts for improved performance

--- models/HRM(Original)/HRM-main/script/train/train_hrm_sudoku_1gpu.sh
+++ models/HRM(Original)/HRM-main/script/train/train_hrm_sudoku_1gpu.sh
@@ -13,10 +13,10 @@ cd ~/HMR/models/HRM\(Original\)/HRM-main
 
 DISABLE_COMPILE=1 OMP_NUM_THREADS=8 python3 pretrain.py \
     data_path=../../../dataset/data/sudoku-extreme-1k-aug-1000 \
-    epochs=20000 \
-    eval_interval=2000 \
-    global_batch_size=384 \
-    lr=7e-5 \
-    puzzle_emb_lr=7e-5 \
+    epochs=40000 \
+    eval_interval=1000 \
+    global_batch_size=768 \
+    lr=1e-4 \
+    puzzle_emb_lr=1e-4 \
     weight_decay=1.0 \
     puzzle_emb_weight_decay=1.0
