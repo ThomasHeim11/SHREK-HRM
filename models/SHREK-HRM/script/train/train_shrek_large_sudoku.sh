c5ab6ece7b532c568aacd0bcc90d25029366b6a4
Thomas Heim <wearedreamin@gmail.com>
2026-03-24 16:30:59 +0100

    fix: Update training scripts for shrek_large and shrek_tiny models with correct parameters

--- models/SHREK-HRM/script/train/train_shrek_large_sudoku.sh
+++ models/SHREK-HRM/script/train/train_shrek_large_sudoku.sh
@@ -11,18 +11,16 @@ module load cuda12.6/toolkit/12.6.3
 
 cd ~/HMR/models/SHREK-HRM
 
-DISABLE_COMPILE=1 OMP_NUM_THREADS=8 python3 pretrain.py \
+OMP_NUM_THREADS=8 python3 pretrain.py \
     arch=shrek_large \
     data_path=../../dataset/data/sudoku-extreme-1k-aug-1000-hint \
     epochs=40000 \
     eval_interval=1000 \
-    global_batch_size=384 \
-    lr=7e-5 \
-    puzzle_emb_lr=7e-5 \
+    global_batch_size=768 \
+    lr=1e-4 \
+    puzzle_emb_lr=1e-4 \
     weight_decay=1.0 \
     puzzle_emb_weight_decay=1.0 \
     +ema=True \
     +run_name=SHREK_Large_Sudoku \
     +project_name=HRM_Sudoku_Comparison
-
-
